14b824a541013a980921370bf4819b97eafb9116
Ananta Podder <ananta8967940712@gmail.com>
2023-03-12 22:01:32 +0530

    added dynamic output.css selection

---
+++ dist/env.js
@@ -0,0 +1,6 @@
+let env="test";
+if(env=="prod"){
+    document.getElementById('styleSheetID').href='./output.css';
+}else{
+    document.getElementById('styleSheetID').href='.././css/output.css';
+}
