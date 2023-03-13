e93f416672a2c3c46407ac15bf1ee8fb471c749e
Rick Kar <rickkar64@gmail.com>
2023-03-14 01:24:17 +0530

    .

--- dist/env.js
+++ dist/env.js
@@ -1,5 +1,5 @@
-let env = "test";
-if (env == "prod") {
+let env = "deploy";
+if (env == "deploy") {
   document.getElementById("styleSheetID").href = "./output.css";
 } else {
   document.getElementById("styleSheetID").href = ".././css/output.css";
